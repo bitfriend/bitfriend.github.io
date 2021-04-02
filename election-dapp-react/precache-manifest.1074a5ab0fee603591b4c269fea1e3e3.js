427f9124ab38fa55dca580874c8324fb141d7d86
bitfriend <bitfriend@hotmail.com>
2021-04-03 07:24:59 +0800

    Add election-dapp-react

---
+++ election-dapp-react/precache-manifest.1074a5ab0fee603591b4c269fea1e3e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9644a24c251e7f7c76add9c796deeed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f49f9121486e9af81f1b",
+    "url": "/static/css/2.9cb0f446.chunk.css"
+  },
+  {
+    "revision": "4edcaac83fdfe2e75e56",
+    "url": "/static/css/main.f1b42223.chunk.css"
+  },
+  {
+    "revision": "f49f9121486e9af81f1b",
+    "url": "/static/js/2.aefc9303.chunk.js"
+  },
+  {
+    "revision": "3c7dfa4a985016317d2b900a73b726f2",
+    "url": "/static/js/2.aefc9303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4edcaac83fdfe2e75e56",
+    "url": "/static/js/main.ce826c7f.chunk.js"
+  },
+  {
+    "revision": "ee31ea13a73850c7a5f2",
+    "url": "/static/js/runtime-main.5e69b1e6.js"
+  }
+]);
