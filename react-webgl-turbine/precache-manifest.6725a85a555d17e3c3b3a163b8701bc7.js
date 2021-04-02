51afae58c08771e7da7d8a69de50fe08d927ff82
bitfriend <bitfriend@hotmail.com>
2021-04-03 07:09:11 +0800

    Refine project structure

---
+++ react-webgl-turbine/precache-manifest.6725a85a555d17e3c3b3a163b8701bc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3855d9fb6472d2870b3",
+    "url": "/static/css/main.71e2b2ed.chunk.css"
+  },
+  {
+    "revision": "b3855d9fb6472d2870b3",
+    "url": "/static/js/main.b3855d9f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5926ffcb3a26cde30adb",
+    "url": "/static/css/2.18d75759.chunk.css"
+  },
+  {
+    "revision": "5926ffcb3a26cde30adb",
+    "url": "/static/js/2.5926ffcb.chunk.js"
+  },
+  {
+    "revision": "b25be90a71b4b9e0307a7130595affd1",
+    "url": "/index.html"
+  }
+];
