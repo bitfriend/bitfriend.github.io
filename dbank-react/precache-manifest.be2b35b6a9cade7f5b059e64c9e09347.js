145a14f9dc0e323eae2acde51a070201fd7289e9
bitfriend <bitfriend@hotmail.com>
2021-04-18 18:00:04 +0300

    Add dbank-react

---
+++ dbank-react/precache-manifest.be2b35b6a9cade7f5b059e64c9e09347.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c9ca5d2c315ce979e4072512ac1627",
+    "url": "/index.html"
+  },
+  {
+    "revision": "723c89dfb4746929a425",
+    "url": "/static/css/2.ef5dd2b0.chunk.css"
+  },
+  {
+    "revision": "55a2874ca539a8bed969",
+    "url": "/static/css/main.1261de62.chunk.css"
+  },
+  {
+    "revision": "723c89dfb4746929a425",
+    "url": "/static/js/2.10b8f6a2.chunk.js"
+  },
+  {
+    "revision": "f531a00174eebc994b2ef755780603e6",
+    "url": "/static/js/2.10b8f6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55a2874ca539a8bed969",
+    "url": "/static/js/main.e4ee6eff.chunk.js"
+  },
+  {
+    "revision": "bf7b91baf9a351c07e2d",
+    "url": "/static/js/runtime-main.9587796f.js"
+  },
+  {
+    "revision": "adca41d3777e83416e7ad919fa0da8ac",
+    "url": "/static/media/logo.adca41d3.png"
+  }
+]);
