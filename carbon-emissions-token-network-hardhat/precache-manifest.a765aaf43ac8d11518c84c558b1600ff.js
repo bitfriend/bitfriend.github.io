98916559f729389044609a344a8d3f504cca8f3b
bitfriend <bitfriend@hotmail.com>
2021-05-12 20:59:01 +0200

    Add Carbon Emissions Token Network project

---
+++ carbon-emissions-token-network-hardhat/precache-manifest.a765aaf43ac8d11518c84c558b1600ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00bcf2c9db2d30638c5b4ecd96464e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da250bfd710a099e324e",
+    "url": "/static/css/2.cf62996a.chunk.css"
+  },
+  {
+    "revision": "f66c75ea61c449622cac",
+    "url": "/static/css/main.8b95a2ec.chunk.css"
+  },
+  {
+    "revision": "da250bfd710a099e324e",
+    "url": "/static/js/2.e93d9ba4.chunk.js"
+  },
+  {
+    "revision": "d76341a167b45a0386ef85ae8a3e5577",
+    "url": "/static/js/2.e93d9ba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66c75ea61c449622cac",
+    "url": "/static/js/main.dcad4b69.chunk.js"
+  },
+  {
+    "revision": "9d9eac67015231eb0c0a",
+    "url": "/static/js/runtime-main.428feb04.js"
+  }
+]);
