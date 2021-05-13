eeabcbc3b8eda62ee0d95fe9d91d5a6af639546e
bitfriend <bitfriend@hotmail.com>
2021-05-13 14:01:57 +0200

    Fix bug in URL problem on gitpage deployment

--- carbon-emissions-token-network-hardhat/service-worker.js
+++ carbon-emissions-token-network-hardhat/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.319881e2a05f8f075e145f39e7d82e69.js"
+  "/carbon-emissions-token-network-hardhat/precache-manifest.446665d4ec0ba42a7ff095814a1e0d26.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/carbon-emissions-token-network-hardhat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
