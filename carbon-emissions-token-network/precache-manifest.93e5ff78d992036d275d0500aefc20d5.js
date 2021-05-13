3a4cf0e3aef38beceef8e19c88bbb3b9629302b9
bitfriend <bitfriend@hotmail.com>
2021-05-13 20:58:45 +0200

    Add carbon project

---
+++ carbon-emissions-token-network/precache-manifest.93e5ff78d992036d275d0500aefc20d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f7320d5ad29df27016479d271da523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a87e379db5a4a076e86",
+    "url": "/static/css/2.cf62996a.chunk.css"
+  },
+  {
+    "revision": "b2ffa9e858149711c4c6",
+    "url": "/static/css/main.6569f3b6.chunk.css"
+  },
+  {
+    "revision": "6a87e379db5a4a076e86",
+    "url": "/static/js/2.cdce20e5.chunk.js"
+  },
+  {
+    "revision": "d76341a167b45a0386ef85ae8a3e5577",
+    "url": "/static/js/2.cdce20e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ffa9e858149711c4c6",
+    "url": "/static/js/main.b5df9ad7.chunk.js"
+  },
+  {
+    "revision": "9d9eac67015231eb0c0a",
+    "url": "/static/js/runtime-main.428feb04.js"
+  }
+]);
