198bed635c95cae782806833bb7979e747228932
bitfriend <bitfriend@hotmail.com>
2021-05-30 22:31:51 +0200

    Fix bug in image URL on github deploy

--- closecross-trades-react/service-worker.js
+++ closecross-trades-react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ab5892001a85cc5e91442b6463c7c17.js"
+  "./precache-manifest.baea876f4d6d4dccda379ff83c41e3a3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
