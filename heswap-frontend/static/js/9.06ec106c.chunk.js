fded155b0b982af2105aaea2026aa641b607319f
bitfriend <bitfriend@hotmail.com>
2021-07-13 00:23:25 +0200

    Implement new logo design in Farm/Pool/Dice pages

--- heswap-frontend/static/js/9.06ec106c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpheswap-frontend"]=this["webpackJsonpheswap-frontend"]||[]).push([[9],{872:function(n,p){}}]);
-//# sourceMappingURL=9.06ec106c.chunk.js.map
---
+++ heswap-frontend/static/js/9.bfb4ad92.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpheswap-frontend"]=this["webpackJsonpheswap-frontend"]||[]).push([[9],{874:function(n,p){}}]);
+//# sourceMappingURL=9.bfb4ad92.chunk.js.map
